71f62e48383e37917d0a35a909c754a0fe277f9c
yaghut.nakhoda <yaghut.nakhoda@gmail.com>
2022-06-02 07:31:09 +0430

    first commit- add first migration and table

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::group(['middleware' => 'auth'], function () {
-   Route::get('/dashboard',[AdminController::class,'index'])->name('dashboard');
-});
+require __DIR__.'/auth.php';
